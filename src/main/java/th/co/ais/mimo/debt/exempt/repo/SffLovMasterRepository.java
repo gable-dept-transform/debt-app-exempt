326f40de60853484bdd8c7107e73b8965ee8f672
Waroon Larpsrisawad <007343@g-able.com>
2024-06-13 15:48:37 +0700

    [DMREM001] Edit sql AccountSubCate

--- src/main/java/th/co/ais/mimo/debt/exempt/repo/SffLovMasterRepository.java
+++ src/main/java/th/co/ais/mimo/debt/exempt/repo/SffLovMasterRepository.java
@@ -11,22 +11,25 @@ import th.co.ais.mimo.debt.exempt.entity.SffLovMaster;
 
 public interface SffLovMasterRepository extends JpaRepository<SffLovMaster, String> {
 
-    @Query(value = " select s.lov_name name,s.display_val val, s.lov_name||' : '||s.display_val AS label" +
-            "  FROM {h-schema}SFF_LOV_MASTER s " +
-            "  where  s.LOV_TYPE = :lovType" +
-            " and s.active_flg = 'Y'  " +
-            " order by s.lov_name ", nativeQuery = true)
-    List<CommonCheckListDto> getLovByLovType(@Param("lovType") String lovType) throws Exception;
+        @Query(value = " select s.lov_name name,s.display_val val, s.lov_name||' : '||s.display_val AS label" +
+                        "  FROM {h-schema}SFF_LOV_MASTER s " +
+                        "  where  s.LOV_TYPE = :lovType" +
+                        " and s.active_flg = 'Y'  " +
+                        " order by s.lov_name ", nativeQuery = true)
+        List<CommonCheckListDto> getLovByLovType(@Param("lovType") String lovType) throws Exception;
 
-    @Query(value = " select sm.lov_name name,sm.display_val val, s.lov_name||' / '||sm.lov_name||' : '||sm.display_val AS label " +
-            "  FROM {h-schema}SFF_LOV_MASTER s, {h-schema}sff_lov_master sm  " +
-            "  where s.row_id = sm.par_row_id  " +
-            " and s.lov_type = :lovType " +
-            " and sm.lov_type = :subLovType " +
-            " and s.active_flg = 'Y'  " +
-            " and sm.active_flg = 'Y' " +
-            " and s.lov_name = :category " +
-            " order by sm.lov_name ", nativeQuery = true)
-    List<CommonCheckListDto> getSubLovByLovTypeandSublovTypeandCategory(@Param("lovType") String lovType,
-            @Param("subLovType") String subLovType, @Param("category") String category) throws Exception;
+        @Query(value = " select " +
+                        "s.lov_name || ' / ' || sm.lov_name || ' : ' || sm.display_val AS label, " +
+                        "s.lov_name || ' / ' || sm.lov_name as name, " +
+                        "sm.display_val val " +
+                        "  FROM {h-schema}SFF_LOV_MASTER s, {h-schema}sff_lov_master sm  " +
+                        "  where s.row_id = sm.par_row_id  " +
+                        " and s.lov_type = :lovType " +
+                        " and sm.lov_type = :subLovType " +
+                        " and s.active_flg = 'Y'  " +
+                        " and sm.active_flg = 'Y' " +
+                        " and s.lov_name = :category " +
+                        " order by sm.lov_name ", nativeQuery = true)
+        List<CommonCheckListDto> getSubLovByLovTypeandSublovTypeandCategory(@Param("lovType") String lovType,
+                        @Param("subLovType") String subLovType, @Param("category") String category) throws Exception;
 }
